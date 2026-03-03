6c92c748873af4b55412800d754480f87252d128
wannabe-geek <vsryashwanth.007@gmail.com>
2026-03-03 09:53:25 +0530

    Removed Week 2 folder

--- Week 2/Level1/KmtoMiles.java
+++
@@ -1,8 +0,0 @@
-public class KmtoMiles{
-	public static void main(String[] args){
-		double kilometre = 10.8;
-		double miles = kilometre / 1.6;
-		
-		System.out.println("The distance " + kilometre + "km in miles is " + miles);
-	}
-}
